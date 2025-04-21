e78a28decb5a95558609bc659bf92b6a8a531762
hana khyy <emailbaru@example.com>
2025-04-21 08:45:41 +0700

    perbaikan tampilan

--- Backend/quickcount.php
+++ Backend/quickcount.php
@@ -66,7 +66,7 @@ $data_json = json_encode($data);
                 <path fill="#0066FF"
                     d="M13 8V4q0-.425.288-.712T14 3h6q.425 0 .713.288T21 4v4q0 .425-.288.713T20 9h-6q-.425 0-.712-.288T13 8M3 12V4q0-.425.288-.712T4 3h6q.425 0 .713.288T11 4v8q0 .425-.288.713T10 13H4q-.425 0-.712-.288T3 12m10 8v-8q0-.425.288-.712T14 11h6q.425 0 .713.288T21 12v8q0 .425-.288.713T20 21h-6q-.425 0-.712-.288T13 20M3 20v-4q0-.425.288-.712T4 15h6q.425 0 .713.288T11 16v4q0 .425-.288.713T10 21H4q-.425 0-.712-.288T3 20m2-9h4V5H5zm10 8h4v-6h-4zm0-12h4V5h-4zM5 19h4v-2H5zm4-2" />
             </svg>
-            <a href="dashboardser.php">Dashboard</a>
+            <a href="../Backend/dashboardser.php">Dashboard</a>
         </div>
     </header>
 
@@ -75,7 +75,7 @@ $data_json = json_encode($data);
     </section>
 
     <section class="data">
-        <div style="width: 50%; margin: auto;">
+        <div style="width: 30%; margin: auto;">
             <canvas id="myChart"></canvas>
         </div>
 
